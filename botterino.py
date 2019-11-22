f52c0f2d5f580fd5b7d6b2ffa058354e5caa6493
seintzx <seintzx@gmail.com>
2019-11-22 17:53:49 +0100

    bot command module

---
+++ botterino.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+move elif cascade from main here
+
+function that return a dictionary with 2 elements:
+    1) cmd to run
+    2) message to send
+
+def check_command(command):
+    if
+    elif
+    elif
+    ...
+
+    return dic
+"""
