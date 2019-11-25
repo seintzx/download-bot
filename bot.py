3c3d9c95f3101b5c191c2a03182e0e5ca547803c
seintzx <seintzx@gmail.com>
2019-11-25 17:23:55 +0100

    refactor structure

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from telepot.loop import MessageLoop
-import commander
+import botterino
 import configuration
 import os
 import sys
@@ -20,40 +20,7 @@ def action(msg):
     print("In chat with " + str(chat_id))
     print("Received: {0}".format(command))
 
-    if command == '/hi':
-        telegram_bot.sendMessage(chat_id, str("Hi! I'm Daniele's Assistant"))
-    elif command == '/time':
-        telegram_bot.sendMessage(chat_id,
-                                 str(now.hour) + str(":") + str(now.minute))
-    elif command == '/troff':
-        commander.run_cmd('sudo service transmission-daemon stop')
-        telegram_bot.sendMessage(chat_id, str('transmission stopped!'))
-    elif command == '/tron':
-        commander.run_cmd('sudo service transmission-daemon start')
-        telegram_bot.sendMessage(chat_id, str('transmission started!'))
-    elif command == '/minirst':
-        commander.run_cmd('sudo service minidlna force-reload')
-        telegram_bot.sendMessage(chat_id, str('minidlna restarted!'))
-    elif command.split(' ')[0] == '/tradd':
-        magnet = command.split(' ')[1]
-        telegram_bot.sendMessage(chat_id,
-                                 str('Adding "' + magnet + '" to dowloads'))
-        commander.run_cmd(
-            'transmission-remote --auth transmission:transmission -a "' +
-            magnet + '"')
-        telegram_bot.sendMessage(chat_id, str('Torrent added to downloads!'))
-    elif command == '/trlist':
-        trlist = commander.run_cmd(
-            'transmission-remote --auth transmission:transmission -l')
-        telegram_bot.sendMessage(chat_id, trlist)
-    elif command == '/top10ps':
-        top10ps = commander.run_cmd(
-            'ps -ax --sort=-pcpu -o user,pid,%cpu,%mem,start,time,cmd | head -n 10'
-        )
-        telegram_bot.sendMessage(chat_id, top10ps)
-    elif command == '/avail':
-        df_sda1 = commander.run_cmd('df -h /dev/sda1')
-        telegram_bot.sendMessage(chat_id, df_sda1)
+    telegram_bot.sendMessage(chat_id, botterino.run_cmd(command))
 
 
 if __name__ == "__main__":
